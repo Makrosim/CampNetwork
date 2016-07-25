0e2f6abaf3dea11b7f88c35a1da0b6da062f98c7
VSKlo <VSKlo@DESKTOP-D2O2SD9>
2016-07-25 12:40:23 +0300

    Add project files.

---
+++ CampBusinessLogic/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CampBusinessLogic
+{
+    public class Class1
+    {
+    }
+}
